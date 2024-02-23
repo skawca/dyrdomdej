d16588c5721e2ad0c96e67343a1dd8cc1f73c87b
skawca <161016666+skawca@users.noreply.github.com>
2024-02-23 16:09:45 +0000

    Deploying to gh-pages from main @ 2d8b3635d40888196313e1dd8297f15a38efcde1 🚀

--- src/akcije/kirbs.ts
+++
@@ -1,6 +0,0 @@
-export const kirbs = () => {
-    WA.room.onEnterLayer('k-kirbs').subscribe(() => {
-        WA.chat.sendChatMessage('Njestup na kirbs toho pinčnika! Z kirbsa zhotowi słódnu kirbsowu poliwku.', 'spodźiwny hłós z nihdźe')
-        }
-    )
-    }
--- src/akcije/teleport-simon.ts
+++
@@ -1,6 +0,0 @@
-export const teleport = () => {
-    WA.room.onEnterLayer('Smn-Teleport').subscribe(() => {
-        WA.chat.sendChatMessage(`Super, sy so Teleportowaw.`, 'Günter')
-        WA.nav.goToRoom('map2.tmj#Smn-cil')
-    })
-}
