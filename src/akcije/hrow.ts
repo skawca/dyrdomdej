2b96394c182e0714f2e51e50236156d7924b74f7
Kilian <tablettonisaekbautzen@gmail.com>
2024-02-23 15:59:40 +0100

    mjeno mortwych+wobydlerjo

--- src/akcije/hrow.ts
+++ src/akcije/hrow.ts
@@ -1,11 +1,11 @@
 export const hrow = () => {
     WA.room.onEnterLayer('k-hrow1').subscribe(() => {
-        WA.chat.sendChatMessage('Daj mje spać.', 'Mortwy')
+        WA.chat.sendChatMessage('Daj mje spać.', 'Mortwy Felix')
         }
     )
 
     WA.room.onEnterLayer('k-hrow2').subscribe(() => {
-        WA.chat.sendChatMessage('Nimaš raz wjace w rowje měr!?.', 'Mortwy')
+        WA.chat.sendChatMessage('Nimaš raz wjace w rowje měr!?.', 'Mortwy Michał')
         }
     )
     }
---
+++ src/akcije/wobydler.ts
@@ -0,0 +1,14 @@
+export const hrow = () => {
+    WA.room.onEnterLayer('k-wobydler1').subscribe(() => {
+        WA.chat.sendChatMessage('Jowe zno nichtu bydli!', 'Wobydler Kilian')
+        }
+    )
+
+    WA.room.onEnterLayer('k-wobydler2').subscribe(() => {
+        WA.chat.sendChatMessage('Jowe tejš!', 'Wobydler Simon')
+        }
+    )
+    }
+
+    
+    
