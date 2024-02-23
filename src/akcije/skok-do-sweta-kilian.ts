9cedd2e43d57c8ebc2cd2ea14ec0c0948f37cbcf
MiC <info@lucija.de>
2024-02-23 13:32:46 +0100

    skoksdfk adfgjhsg

--- src/akcije/skok-do-sweta-kilian.ts
+++ src/akcije/skok-do-sweta-kilian.ts
@@ -1,9 +1,17 @@
-export const skocDoSwetaKilian = () => {
+export const skocDoDruhehoSweta = () => {
+
     WA.room.onEnterLayer('mapKilian').subscribe(() => {
         WA.nav.goToRoom('Kilian.tmj')
     })
 
     WA.room.onEnterLayer('mapGustav').subscribe(() => {
         WA.nav.goToRoom('map2.tmj')
+        // map2.tmj je wot Simona
     })
+
+    WA.room.onEnterLayer('MiC-SprungKilian').subscribe(() => {
+        WA.nav.goToRoom('Kilian.tmj')
+    })
+
+    
 }
